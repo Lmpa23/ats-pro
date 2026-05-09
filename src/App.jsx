a5df3ffab3f71f060df67cd45949a2ffc216cd28
Loreys <loreysmilena@hotmail.com>
2026-05-09 17:57:46 -0500

    Fix final real syntax

--- src/App.jsx
+++ src/App.jsx
@@ -236,7 +236,7 @@ const guardar = async () => {
     ]
   }));
 setForm(null);
-   };
+     };
  if(form) return (
     <div className="max-w-2xl mx-auto">
       <div className="flex items-center justify-between mb-4">
